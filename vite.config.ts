400a81c809b07dd8be4e081779bd5798a7dd54f0
fedeshindiaz <281490349+fedeshindiaz@users.noreply.github.com>
2026-07-28 13:15:22 -0300

    Restaurar acceso autenticado beta 43

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
-        cacheId: 'onur-beta-0.1.0-beta.42',
+        cacheId: 'onur-beta-0.1.0-beta.43',
         globIgnores: ['**/ocr/**', '**/three.module-*.js'],
         cleanupOutdatedCaches: true,
         skipWaiting: true,
